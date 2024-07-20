3d104ad808830651d2b805b184c5102f6a78927e
Jonas Steen Hansen <jonas@jonas-hansen.dk>
2024-07-21 00:09:29 +0200

    Installing the framework and setting up defaults

--- defaults.sh
+++ defaults.sh
@@ -1,2 +1,6 @@
+# Setup defaults
+export DEVSTATION_SETUP_HOME="~/local/share/devstation-setup/"
+
+# GIT
 export GIT_NAME="Jonas Steen Hansen"
 export GIT_EMAIL="jonas@jonas-hansen.dk"
--- install.sh
+++ install.sh
@@ -1,1 +1,6 @@
-source defaults.sh # Setting ENV vars
+echo "### Setting installing framework"
+ln -sf ~/devstation-setup ~/local/share/devstation-setup
+
+echo "### Setting up defaults"
+echo "source ~/local/share/devstation-setup/defaults.sh" > ~/.bashrc # Setting ENV vars
+source ~/.bashrc
