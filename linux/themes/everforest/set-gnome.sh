fecd3b7ee8b923a496dbe4cd50107f79ca92d435
Jonas Steen Hansen <jonas@jonas-hansen.dk>
2024-09-13 22:28:42 +0200

    Setup everforest theme

---
+++ linux/themes/everforest/set-gnome.sh
@@ -0,0 +1,6 @@
+gsettings set org.gnome.desktop.interface color-scheme 'prefer-dark'
+gsettings set org.gnome.desktop.interface cursor-theme 'Yaru'
+gsettings set org.gnome.desktop.interface gtk-theme "Yaru-bark-dark"
+gsettings set org.gnome.desktop.interface icon-theme "Yaru-bark"
+
+gsettings set org.gnome.desktop.background picture-options 'zoom'
