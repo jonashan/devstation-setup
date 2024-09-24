b413374de18f3649a52360b593da49b68380f209
Jonas Steen Hansen <jonas@jonas-hansen.dk>
2024-09-24 13:25:21 +0200

    Added imagemagick for rmagick

--- linux/install/terminal/libraries.sh
+++ linux/install/terminal/libraries.sh
@@ -3,4 +3,4 @@ sudo apt install -y \
   build-essential pkg-config bison clang rustc golang \
   gir1.2-gtop-2.0 gir1.2-clutter-1.0 \
   zlib1g-dev libssl-dev libreadline-dev zlib1g-dev libyaml-dev libreadline-dev libncurses5-dev libffi-dev libgdbm-dev libjemalloc2 \
-  ripgrep
+  ripgrep imagemagick libmagickwand-dev
