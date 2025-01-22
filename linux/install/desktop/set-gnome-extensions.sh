fea188ed29912b2514d58204fe3393bb295e8ab2
Jonas Hansen <jonas@jonas-hansen.dk>
2025-01-22 14:37:08 +0100

    Removed Gum

--- linux/install/desktop/set-gnome-extensions.sh
+++ linux/install/desktop/set-gnome-extensions.sh
@@ -8,9 +8,6 @@ gnome-extensions disable ubuntu-appindicators@ubuntu.com
 gnome-extensions disable ubuntu-dock@ubuntu.com
 gnome-extensions disable ding@rastersoft.com
 
-# Pause to assure user is ready to accept confirmations
-gum confirm "To install Gnome extensions, you need to accept some confirmations. Are you ready?"
-
 # Install new extensions
 gext install tactile@lundal.io
 gext install just-perfection-desktop@just-perfection
