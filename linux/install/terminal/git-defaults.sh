17054a23567279b7ac75c095d39cc72df3d83681
Jonas Hansen <jonas@jonas-hansen.dk>
2025-03-21 15:32:35 +0100

    Fixing git defaults as string

--- linux/install/terminal/git-defaults.sh
+++ linux/install/terminal/git-defaults.sh
@@ -1,3 +1,3 @@
-git config --global user.name Jonas Steen Hansen
-git config --global user.email jonas@jonas-hansen.dk
+git config --global user.name "Jonas Steen Hansen"
+git config --global user.email "jonas@jonas-hansen.dk"
 git config --global pull.rebase true
